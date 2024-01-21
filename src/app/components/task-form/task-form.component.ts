3b5ea8a9d711a6e6f43ff0f552a55813df12611c
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-01-20 22:04:07 -0300

    feat: submitting tasks to localStorage

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,6 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
+//Enums
+import { ETaskPriority } from '@enums/task-priority.enum';
+
+//Services
+import { FormatDateService } from '@services/formatDate/format-date.service';
+import { LocalStorageService } from '@services/localStorage/local-storage.service';
+
 @Component({
     selector: 'app-task-form',
     standalone: true,
@@ -11,28 +18,66 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class TaskFormComponent {
     #fb = inject(FormBuilder);
+    #localStorageService = inject(LocalStorageService);
+    #formatDate = inject(FormatDateService);
 
     public taskForm = this.#fb.group({
+        id: new Date().getTime(),
+        creationDate: new Date().toLocaleDateString(),
         title: ['', [Validators.required]],
         subject: [''],
         description: [''],
-        priority: ['normal'],
-        deadlineDate: [''],
+        priority: [ETaskPriority.DEFAULT],
+        deadlineDate: '',
     });
+
+    #formatDeadlineDate() {
+        if (this.taskForm.get('deadlineDate')?.value) {
+            const deadlineDate = this.taskForm.get('deadlineDate');
+            const date = deadlineDate!.value;
+
+            const formatedDate = this.#formatDate.stringToLocaleDateString(
+                date!
+            );
+
+            deadlineDate?.setValue(formatedDate);
+        } else {
+            this.taskForm.get('deadlineDate')?.setValue(null);
+        }
+    }
+
+    #submitToLocalStorage() {
+        const newTask = this.taskForm.value;
+
+        const prevTasksList =
+            this.#localStorageService.getLocalStorageItem('tasksList');
+
+        if (prevTasksList) {
+            this.#localStorageService.setLocalStorageItem('tasksList', [
+                ...prevTasksList,
+                newTask,
+            ]);
+        } else {
+            this.#localStorageService.setLocalStorageItem('tasksList', [
+                newTask,
+            ]);
+        }
+    }
 
     #clearTaskForm() {
         this.taskForm.patchValue({
             title: '',
             subject: '',
             description: '',
-            priority: 'normal',
+            priority: ETaskPriority.DEFAULT,
             deadlineDate: '',
         });
     }
 
     handleSubmitTaskForm() {
         if (this.taskForm.valid) {
-            console.log(this.taskForm.value);
+            this.#formatDeadlineDate();
+            this.#submitToLocalStorage();
             this.#clearTaskForm();
         }
     }
