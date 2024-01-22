e94dfe1988e5025b83bceafc84b156c343254967
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-01-22 14:47:59 -0300

    removing unused code

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
+//Interfaces
+import { ITask } from '@interfaces/task.interface';
+
 //Enums
 import { ETaskPriority } from '@enums/task-priority.enum';
-import { ITask } from '@interfaces/task.interface';
 
 //Services
 import { FormatDateService } from '@services/formatDate/format-date.service';
-import { LocalStorageService } from '@services/localStorage/local-storage.service';
 import { TasksListService } from '@services/tasksList/tasks-list.service';
 
 @Component({
@@ -20,7 +21,6 @@ import { TasksListService } from '@services/tasksList/tasks-list.service';
 })
 export class TaskFormComponent {
     #fb = inject(FormBuilder);
-    #localStorageService = inject(LocalStorageService);
     #tasksListService = inject(TasksListService);
     #formatDate = inject(FormatDateService);
 
@@ -45,24 +45,6 @@ export class TaskFormComponent {
             deadlineDate?.setValue(formatedDate);
         } else {
             deadlineDate?.setValue(null);
-        }
-    }
-
-    #submitToLocalStorage() {
-        const newTask = this.taskForm.value;
-
-        const prevTasksList =
-            this.#localStorageService.getLocalStorageItem('tasksList');
-
-        if (prevTasksList) {
-            this.#localStorageService.setLocalStorageItem('tasksList', [
-                ...prevTasksList,
-                newTask,
-            ]);
-        } else {
-            this.#localStorageService.setLocalStorageItem('tasksList', [
-                newTask,
-            ]);
         }
     }
 
