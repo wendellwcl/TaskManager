ba8fc48ae2c2e241dcf885568305aeba7ad12a6c
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-01-31 22:45:15 -0300

    refactor: modifying how the task-list-render component receives task list data

--- src/app/components/task-list-render/task-list-render.component.ts
+++ src/app/components/task-list-render/task-list-render.component.ts
@@ -1,22 +1,16 @@
 import {
     ChangeDetectionStrategy,
     Component,
-    OnDestroy,
-    OnInit,
-    inject,
+    Input,
     signal,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 //Interfaces
 import { ITask } from '@interfaces/task.interface';
 
 //Components
 import { TaskItemComponent } from '@components/task-item/task-item.component';
-
-//Services
-import { TasksListService } from '@services/tasksList/tasks-list.service';
 
 @Component({
     selector: 'app-task-list-render',
@@ -26,20 +20,10 @@ import { TasksListService } from '@services/tasksList/tasks-list.service';
     styleUrl: './task-list-render.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskListRenderComponent implements OnInit, OnDestroy {
-    #tasksListService = inject(TasksListService);
-    #tasksListSubscription?: Subscription;
+export class TaskListRenderComponent {
+    @Input({ required: true }) set inputListRender(value: ITask[] | null) {
+        this.tasksList.set(value);
+    }
 
     public tasksList = signal<ITask[] | null>(null);
-
-    ngOnInit(): void {
-        this.#tasksListSubscription =
-            this.#tasksListService.getTasksListRender$.subscribe((value) =>
-                this.tasksList.set(value)
-            );
-    }
-
-    ngOnDestroy(): void {
-        this.#tasksListSubscription?.unsubscribe();
-    }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    OnDestroy,
+    OnInit,
+    inject,
+    signal,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 //Components
 import { DashboardHeaderComponent } from '@components/dashboard-header/dashboard-header.component';
 import { TaskListRenderComponent } from '@components/task-list-render/task-list-render.component';
+import { ITask } from '@interfaces/task.interface';
+
+//Services
+import { TasksListService } from '@services/tasksList/tasks-list.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -12,4 +24,20 @@ import { TaskListRenderComponent } from '@components/task-list-render/task-list-
     styleUrl: './dashboard.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit, OnDestroy {
+    #tasksListService = inject(TasksListService);
+    #tasksListSubscription?: Subscription;
+
+    public tasksLists = signal<ITask[] | null>(null);
+
+    ngOnInit(): void {
+        this.#tasksListSubscription =
+            this.#tasksListService.getTasksListRender$.subscribe((value) => {
+                this.tasksLists.set(value);
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.#tasksListSubscription?.unsubscribe();
+    }
+}
