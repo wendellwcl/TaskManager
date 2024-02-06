e122104c3853dd244f4ddfdac553879bcf85c86f
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-02-05 22:00:34 -0300

    feat: organizing the history page

--- src/app/pages/historic/historic.component.ts
+++ src/app/pages/historic/historic.component.ts
@@ -1,10 +1,12 @@
 import {
     ChangeDetectionStrategy,
     Component,
+    OnDestroy,
     OnInit,
     inject,
     signal,
 } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 //Components
 import { TaskListRenderComponent } from '@components/task-list-render/task-list-render.component';
@@ -23,14 +25,22 @@ import { TasksListService } from '@services/tasksList/tasks-list.service';
     styleUrl: './historic.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HistoricComponent implements OnInit {
+export class HistoricComponent implements OnInit, OnDestroy {
     #tasksListService = inject(TasksListService);
+    #historicSubscription = new Subscription();
 
     public tasksHistoric = signal<ITask[] | null>(null);
 
     ngOnInit(): void {
-        this.#tasksListService.getTasksHistoric$.subscribe((value) =>
-            this.tasksHistoric.set(value)
-        );
+        //Subscribing to get the data from tasksHistoric
+        this.#historicSubscription =
+            this.#tasksListService.getTasksHistoric$.subscribe((value) =>
+                this.tasksHistoric.set(value)
+            );
+    }
+
+    ngOnDestroy(): void {
+        //Unsubscribing from tasksHistoric
+        this.#historicSubscription.unsubscribe();
     }
 }
