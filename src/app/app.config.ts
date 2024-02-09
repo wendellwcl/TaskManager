e60bc2801027bb3318a942c946ad6df4493bd2ae
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-02-09 14:34:11 -0300

    feat: add HashLocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+    provideRouter,
+    withComponentInputBinding,
+    withHashLocation,
+} from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()],
+    providers: [
+        provideRouter(routes, withHashLocation(), withComponentInputBinding()),
+        provideAnimations(),
+    ],
 };
